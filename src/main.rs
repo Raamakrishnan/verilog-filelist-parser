8d53cff390e4eae78518cd1b867fc07007dca17f
Dave McEwan <dave.mcewan@bristol.ac.uk>
2022-02-04 19:48:06 +0000

    helperUtility Implement and include debug-aid based on README example.
    This is useful when debugging complex filelists.
    The README provides most of this example, so just make it usable.
    Output in YAML format which is both human-readable and machine-readable.

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use clap::Parser;
+use std::path::PathBuf;
+use verilog_filelist_parser;
+
+#[derive(Debug, Parser)]
+#[clap(name = "verilog-filelist-parser")]
+#[clap(long_version(option_env!("LONG_VERSION").unwrap_or(env!("CARGO_PKG_VERSION"))))]
+pub struct Opt {
+    #[clap()]
+    pub filelist: Vec<PathBuf>,
+}
+
+pub fn main() {
+    let opt: Opt = Parser::parse();
+
+    for filename in &opt.filelist {
+        println!("{:?}:", filename);
+        let filelist = verilog_filelist_parser::parse_file(filename).expect("Cannot read filelist");
+
+        println!("  files:");
+        for file in filelist.files {
+            println!("    - {:?}", file);
+        }
+
+        println!("  incdirs:");
+        for incdir in filelist.incdirs {
+            println!("    - {:?}", incdir);
+        }
+
+        println!("  defines:");
+        for (d, t) in filelist.defines {
+            match t {
+                None => println!("    - {:?}", d),
+                Some(te) => println!("    - {:?}: {:?}", d, te),
+            };
+        }
+    }
+}
